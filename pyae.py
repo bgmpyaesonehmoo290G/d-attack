c74174a5a25365ad8517895366c3691ee2c941bb
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-23 12:09:24 +0630

    Update pyae.py

--- pyae.py
+++ pyae.py
@@ -28,13 +28,13 @@ os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[====$               ] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[=========$          ] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[==============$     ] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[===================$] 100%"
 time.sleep(3)
 sent = 0
 while True:
